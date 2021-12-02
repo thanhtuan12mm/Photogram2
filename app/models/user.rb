cb8c81be277cf0b156c5ea5cca1f1b142cf6f1c3
thanhtuan12mm <thanhtuan12mm@gmail.com>
2021-12-02 15:03:54 +0700

    Part 4 Profiles edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
   validates :user_name, presence: true, length: { minimum: 4, maximum: 16 }
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_one_attached :avatar
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_29_154247) do
+ActiveRecord::Schema.define(version: 2021_12_02_073425) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2021_11_29_154247) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "user_name"
+    t.text "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["user_name"], name: "index_users_on_user_name", unique: true
